2458c90992827c3305e402fe55ff8ff162eb6e24
Lisa Herzberg <lisaherzberg@Lisas-MacBook-Pro.local>
2017-12-12 12:51:47 -0700

    in class project

--- demo/src/components/About/About.js
+++ demo/src/components/About/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Route, NavLink} from 'react-router-dom';
 
 export default function About(props) {
   return (
     <div>
       <h1>This is the About component</h1>
-      <h3>FAQ</h3>
-      <h3>Company</h3>
+      <NavLink activeClassName="red" to="/About/faq">FAQ</NavLink>
+      <NavLink activeClassName="blue" to="/About/company">Company</NavLink>
       {props.children}
     </div>
   )
--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
 import People from './People/People';
+import FAQ from './About/FAQ/FAQ'
+import Company from './About/Company/Company'
+import Character from './Character/Character'
 
 class App extends Component {
   render() {
@@ -15,8 +18,16 @@ class App extends Component {
             <Link to="/people">People</Link>
         </div>
         <Route exact path= "/" component={Home}/>
-        <Route path= "/About" component={About}/>
-        <Route path= "/People" component={People}/>
+        <Route path= "/about" render={()=>
+          <About>
+              <Route path="/about/faq" component={FAQ}/>
+              <Route path="/about/company" component={Company}/>
+          </About>
+          }/>
+          <Switch>
+            <Route path= "/People/:id" component={Character}/>
+            <Route path= "/People" component={People}/>
+          </Switch>
       </div>
     );
   }
